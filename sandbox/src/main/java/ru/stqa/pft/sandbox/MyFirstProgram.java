c861e4bf6de977e10c18de149f57ae4c7592b1c8
gulnaramurphy7 <murphy.gulnara@gmail.com>
2017-02-22 23:55:53 -0500

    Learning variables

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,32 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
+        String somebody = "world";
         System.out.println("Hello, world!");
+        System.out.println("Hello, " + somebody + "!");
+
+        double l = 8.0;
+        double s = l * l;
+        System.out.println("Plow'ad' kvadrata so storonoj " + l + " = " + s);
+
+
+      //  System.out.println(2 + 2);
+      //  System.out.println(2 * 2);
+      //  System.out.println(2 / 2);
+      //  System.out.println(2 - 2);
+
+      //  System.out.println(1 / 2);
+      //  System.out.println(1.0 / 2);
+      //  System.out.println(1 / 2.0);
+      //  System.out.println(2.0 / 2);
+
+      //  System.out.println("2" + "2");
+      //  System.out.println("2" + 2);
+      //  System.out.println(2 + "2");
+
+      //  System.out.println(2 + 2 * 2);
+      //  System.out.println((2 + 2) * 2);
+      //  System.out.println("2 + 2 = " + (2 + 2));
     }
 
 }
