70d83dcef938e5208ad1b39f157d45d529ebb275
gulnaramurphy7 <murphy.gulnara@gmail.com>
2017-02-23 23:15:24 -0500

    Dobavleno neskolko fynkzij

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,16 +3,38 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        String somebody = "world";
-        System.out.println("Hello, world!");
+        hello("world");
+        hello("user");
+        hello("Gulnara");
+
+        double l = 5;
+        System.out.println("Plowad' kvadrata so storonoj " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Plowad' pramoygolnika so storonami " + a + " i " + b + " = " + area(a, b));
+    }
+
+    public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
+    }
 
-        double l = 8.0;
-        double s = l * l;
-        System.out.println("Plow'ad' kvadrata so storonoj " + l + " = " + s);
+    public static double area(double len) {
+        return len * len;
+    }
 
-        System.out.println("Hello~");
+    public static double area (double a, double b) {
+        return a * b;
+    }
 
+}
+
+        //String somebody = "world";
+        //System.out.println("Hello, " + somebody + "!");
+
+        //double l = 8.0;
+        //double s = l * l;
+        //System.out.println("Plow'ad' kvadrata so storonoj " + l + " = " + s);
 
       //  System.out.println(2 + 2);
       //  System.out.println(2 * 2);
@@ -31,6 +53,5 @@ public class MyFirstProgram {
       //  System.out.println(2 + 2 * 2);
       //  System.out.println((2 + 2) * 2);
       //  System.out.println("2 + 2 = " + (2 + 2));
-    }
 
-}
+
