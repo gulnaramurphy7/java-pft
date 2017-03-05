583190baf37d704a21a597a8fffc61543560facd
gulnaramurphy7 <murphy.gulnara@gmail.com>
2017-03-05 01:06:39 -0500

    Test Framework and first tests

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,24 +3,22 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    //hello("world");
-    //hello("user");
-    //hello("Gulnara");
+    hello("world");
+    hello("user");
+    hello("Gulnara");
 
-    //Square s = new Square(5);
-    //System.out.println("Plowad' kvadrata so storonoj " + s.l + " = " + s.area());
+    Square s = new Square(5);
+    System.out.println("Plowad' kvadrata so storonoj " + s.l + " = " + s.area());
 
-    //Rectangle r = new Rectangle(4, 6);
-    //System.out.println("Plowad' pramoygolnika so storonami " + r.a + " i " + r.b + " = " + r.area());
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Plowad' pramoygolnika so storonami " + r.a + " i " + r.b + " = " + r.area());
+  }
 
-      Point p = new Point(5, 4, 6, 3);
-      System.out.println("Rasstojanie megdy P1 s koordinatami" + p.x1, p.y1 + "i P2 s koordinatami" + p.x2, p.y2 + " = " + p.distance());
-
-
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
   }
 }
-    //public static void hello(String somebody) {
-        //System.out.println("Hello, " + somebody + "!");
+
 
 
 
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by gmurphy on 3/5/2017.
+ */
+//public class MySecondProgram {
+  //public static void main(String[] args) {
+
+    //Point p = new Point(5, 4, 6, 3);
+    //System.out.println("Rasstojanie megdy P1 s koordinatami" + p.x1, p.y1 + "i P2 s koordinatami" + p.x2, p.y2 + " = " + p.distance());
+  //}
+//}
