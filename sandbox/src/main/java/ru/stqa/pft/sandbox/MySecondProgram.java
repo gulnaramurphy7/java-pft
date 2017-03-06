793bf4bd4e5dd1705d3cf2b9152654b77af78c93
gulnaramurphy7 <murphy.gulnara@gmail.com>
2017-03-05 20:09:22 -0500

    Zadanie #2 - peredelannoe

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
@@ -3,10 +3,11 @@ package ru.stqa.pft.sandbox;
 /**
  * Created by gmurphy on 3/5/2017.
  */
-//public class MySecondProgram {
-  //public static void main(String[] args) {
+public class MySecondProgram {
+  public static void main(String[] args) {
 
-    //Point p = new Point(5, 4, 6, 3);
-    //System.out.println("Rasstojanie megdy P1 s koordinatami" + p.x1, p.y1 + "i P2 s koordinatami" + p.x2, p.y2 + " = " + p.distance());
-  //}
-//}
+    Point a = new Point(6, 3);
+    Point b = new Point (5, 4);
+    System.out.println("Rasstojanie megdy to4kami ravno " + a.distance(b));
+  }
+}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -5,19 +5,15 @@ package ru.stqa.pft.sandbox;
  */
 public class Point {
 
-  public double x1;
-  public double y1;
-  public double x2;
-  public double y2;
+  public double x;
+  public double y;
 
-  public Point(double x1, double y1, double x2, double y2) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+  public Point(double x, double y1) {
+    this.x = x;
+    this.y = y;
   }
 
-  public double distance() {
-    return Math.sqrt((this.x1 - this.x2)*(this.x1 - this.x2) + (this.y1 - this.y2) * (this.y1 - this.y2));
+  public double distance(Point a) {
+    return Math.sqrt((a.x - this.x)*(a.x - this.x) + (a.y - this.y) * (a.y - this.y));
   }
 }
