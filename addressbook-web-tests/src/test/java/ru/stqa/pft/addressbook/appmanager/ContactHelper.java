e945043cd23cb3f3fdbab804059f79d0a5387429
gulnaramurphy7 <murphy.gulnara@gmail.com>
2017-03-14 18:42:55 -0400

    Zadanie #6(b): Contact Modification Tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -27,5 +27,24 @@ public class ContactHelper extends HelperBase {
     click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
   }
 
-}
+  public void fillContactForm(ContactData contactData) {
+    type(By.name("contact_firstName"), contactData.getFirstName());
+    type(By.name("contact_lastName"), contactData.getLastName());
+    type(By.name("contact_nickname"), contactData.getNickname());
+    type(By.name("contact_company"), contactData.getCompany());
+    type(By.name("contact_address"), contactData.getAddress());
+    type(By.name("contact_home"), contactData.getHome());
+    type(By.name("contact_mobile"), contactData.getMobile());
+    type(By.name("contact_work"), contactData.getWork());
+    type(By.name("contact_email"), contactData.getEmail());
+  }
 
+
+  public void submitContactModification() {
+    click(By.name("update"));
+  }
+
+  public void returnToHomePage() {
+    click(By.name("home page"));
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 /**
@@ -9,8 +10,11 @@ import ru.stqa.pft.addressbook.model.GroupData;
 public class ContactModificationTests extends TestBase {
 
   @Test
-  public void testContactDeletion() {
+  public void testContactModification() {
     app.getContactHelper().selectContact("8");
     app.getContactHelper().editContact();
+    app.getContactHelper().fillContactForm(new ContactData("test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9" ));
+    app.getContactHelper().submitContactModification();
+    app.getContactHelper().returnToHomePage();
   }
 }
